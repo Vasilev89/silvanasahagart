fe3322edfaeb73150de25f9a7f86b06a5b937513
Antonio Vasilev <antoniovassilev@gmail.com>
2017-12-12 21:27:36 +0000

    :hammer: Adding in SCSS dependecies

--- gulpfile.js
+++ gulpfile.js
@@ -7,9 +7,32 @@ var sass = require('gulp-sass');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
-
+var browserSync = require('browser-sync').create();
 var babel = require('gulp-babel');
 var through = require('through2');
+
+
+// Static Server + watching scss/html files
+gulp.task('serve', ['sass'], function() {
+
+    browserSync.init({
+        server: "./app"
+    });
+
+    gulp.watch("app/scss/*.scss", ['sass']);
+    gulp.watch("app/*.html").on('change', browserSync.reload);
+});
+
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function() {
+    return gulp.src("app/scss/*.scss")
+        .pipe(sass())
+        .pipe(gulp.dest("app/css"))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('default', ['serve']);
+
  
 function logFileHelpers() {
     return through.obj((file, enc, cb) => {
