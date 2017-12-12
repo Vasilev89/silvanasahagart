f7716f3ead9d9b6e1478d926de9509669acb7e70
Antonio Vasilev <antoniovassilev@gmail.com>
2017-12-12 21:11:14 +0000

    Initial Commit

---
+++ gulpfile.js
@@ -0,0 +1,60 @@
+// Include gulp
+var gulp = require('gulp');
+
+// Include Our Plugins
+var jshint = require('gulp-jshint');
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+
+var babel = require('gulp-babel');
+var through = require('through2');
+ 
+function logFileHelpers() {
+    return through.obj((file, enc, cb) => {
+        console.log(file.babel.usedHelpers);
+        cb(null, file);
+    });
+}
+ 
+gulp.task('default', () =>
+    gulp.src('src/**/*.js')
+        .pipe(babel({
+            presets: ['env']
+        }))
+        .pipe(logFileHelpers())
+)
+
+// Lint Task
+gulp.task('lint', function() {
+    return gulp.src('js/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
+});
+
+// Compile Our Sass
+gulp.task('sass', function() {
+    return gulp.src('scss/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('dist/css'));
+});
+
+// Concatenate & Minify JS
+gulp.task('scripts', function() {
+    return gulp.src('js/*.js')
+        .pipe(concat('all.js'))
+        .pipe(gulp.dest('dist'))
+        .pipe(rename('all.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/js'));
+});
+
+// Watch Files For Changes
+gulp.task('watch', function() {
+    gulp.watch('js/*.js', ['lint', 'scripts']);
+    gulp.watch('scss/*.scss', ['sass']);
+});
+
+// Default Task
+gulp.task('default', ['lint', 'sass', 'scripts', 'watch']);
