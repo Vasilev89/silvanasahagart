34b095c15bf1234fa4c20c725ed6627653897bd0
Antonio Vasilev <antoniovassilev@gmail.com>
2018-02-04 16:06:08 +0000

    :100: Adding in Gallery to every button

--- js/core.js
+++ js/core.js
@@ -1,5 +1,40 @@
 $(document).ready(function(){
 
+
+var openPhotoSwipe = function() {
+    var pswpElement = document.querySelectorAll('.pswp')[0];
+
+    // build items array
+    var items = [
+        {
+            src: 'https://farm2.staticflickr.com/1043/5186867718_06b2e9e551_b.jpg',
+            w: 964,
+            h: 1024
+        },
+        {
+            src: 'https://farm7.staticflickr.com/6175/6176698785_7dee72237e_b.jpg',
+            w: 1024,
+            h: 683
+        }
+    ];
+    
+    // define options (if needed)
+    var options = {
+             // history & focus options are disabled on CodePen        
+        history: false,
+        focus: false
+    };
+    
+    var gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, items, options);
+    gallery.init();
+};
+
+$('.ss-init-gallery').click(function(){
+    openPhotoSwipe();
+})
+
+
+
 var initPhotoSwipeFromDOM = function(gallerySelector) {
 
     // parse slide data (url, title, size ...) from DOM elements 
