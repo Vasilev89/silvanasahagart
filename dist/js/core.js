28fd31637d856424a76e0abe49ec000dd69844f3
Antonio Vasilev <antoniovassilev@gmail.com>
2018-02-14 15:28:23 +0000

    :100: General Improvements

--- dist/js/core.js
+++ dist/js/core.js
@@ -7,12 +7,12 @@ var openPhotoSwipe = function() {
     // build items array
     var items = [
         {
-            src: 'https://farm2.staticflickr.com/1043/5186867718_06b2e9e551_b.jpg',
+            src: './/images/Painting1_Large.jpg',
             w: 964,
             h: 1024
         },
         {
-            src: 'https://farm7.staticflickr.com/6175/6176698785_7dee72237e_b.jpg',
+            src: './/images/Painting2_Large.jpg',
             w: 1024,
             h: 683
         }
