a674852fc085ba5276523392dce34c5db16290c2
Antonio Vasilev <antoniovassilev@gmail.com>
2018-02-21 09:52:11 +0000

    :100: Adding in Mobile Nav

--- js/core.js
+++ js/core.js
@@ -240,4 +240,26 @@ var initPhotoSwipeFromDOM = function(gallerySelector) {
 
 // execute above function
 initPhotoSwipeFromDOM('.ss-component-arwtworks-paintings');
+
+// vanilla JavaScript
+document.addEventListener("DOMContentLoaded", function() {
+
+	document.querySelector('.hamburger').addEventListener("click", function() {
+		this.classList.toggle("hamburger--active");
+		document.querySelector(".nav-fullscreen").classList.toggle("nav-fullscreen--open");
+	});
+	
+});
+
+// jQuery
+/*$(document).ready(function() {
+
+    $('.nav-toggle').on('click', function(){
+			$(this).toggleClass('nav-toggle--open');
+			$(".nav-fullscreen").toggleClass("nav-fullscreen--open");
+		});
+
+});
+*/
+
 })
