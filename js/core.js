2e6fc458b2982336126e31b825bf363cfb5667ea
Antonio Vasilev <antoniovassilev@gmail.com>
2018-03-01 14:02:46 +0000

    :100: Adding in Pop_Up

--- js/core.js
+++ js/core.js
@@ -238,6 +238,29 @@ var initPhotoSwipeFromDOM = function(gallerySelector) {
 // execute above function
 initPhotoSwipeFromDOM('.ss-component-arwtworks-paintings');
 
+$('#btnpopup').click(function(e){
+    e.preventDefault();
+})
+
+$('.ss-mobile-only-cta').click(function(e){
+    e.preventDefault();
+})
+
+  // Defaults
+  var BUTTON = document.getElementById('btnpopup');
+  var POPUP = document.getElementById('mypopup');
+  var POPUP_CLASS = 'popup-ui';
+  
+  // Popup button (Click on button to open modal)
+  BUTTON.addEventListener('click', function(){
+    POPUP.className = POPUP_CLASS + ' show';
+  }, false);
+
+  // Popup modal (Click on modal to close it)
+  POPUP.addEventListener('click', function(){
+    this.className = POPUP_CLASS;
+  }, false);
+
 // vanilla JavaScript
 
 $(".menu-link").click(function(event) {
